451f7e5b43108f79f3c8752f21bfcb258128c67b
Amindu <86243749+Amixdu@users.noreply.github.com>
2021-11-15 16:30:02 +0530

    Automated birthday testing on chrome

--- index.js
+++ index.js
@@ -1,83 +1,116 @@
-const {Builder, Key, By, ChromeOptions} = require("selenium-webdriver");
+const {Builder, Key, By} = require("selenium-webdriver");
+
+const { Options: ChromeOptions } = require('selenium-webdriver/chrome');
+const webdriver = require('selenium-webdriver');
 var points = 0;
 
 
 async function testHtmlOpen (){ 
     let driver = new Builder().forBrowser("chrome").build();
-    await driver.get("D:\\Projects\\Selenium\\Test\\index.html");
+    await driver.get("D:\\Projects\\Selenium\\\Test_Chrome\\index.html");
     // await driver.findElement(By.name("q")).sendKeys("Hello", Key.RETURN);
 };
 
 
 // function to test initial birthday prompt
 async function testBirthdayClick (){ 
-    let driver2 = new Builder().forBrowser("chrome").build();
-    await driver2.get("D:\\Projects\\Selenium\\Test\\index.html");
-    await driver2.findElement(By.id("date")).click();
+    // disable cluttering
+    const chromeOptions = new ChromeOptions();
+    chromeOptions.excludeSwitches('enable-logging');
+
+    // open html
+    let driver = await new webdriver.Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(chromeOptions)
+        .build();
+
+    await driver.get("D:\\Projects\\Selenium\\\Test_Chrome\\index.html");
+
+    await driver.findElement(By.id("date")).click();
 
     // check if correct
-    let value = driver2.findElement(By.id("ageOutput")).getText();
+    let value = driver.findElement(By.id("ageOutput")).getText();
     value.then(function(result){
         if (result == "Lemme guess, your age is..."){
             // console.log(result);
             points = points + 1;
-            console.log(points);
+            // console.log(points);
             // printPoints(points);
         }
     })
 
     // driver2.quit();
+    testEnterBirthday();
 };
 
 
 // function to test birthday field prompt when date entered
 async function testEnterBirthday(){
 
+    // disable cluttering
+    const chromeOptions = new ChromeOptions();
+    chromeOptions.excludeSwitches('enable-logging');
+
     // open html
-    let driver3 = new Builder().forBrowser("chrome").build();
-    await driver3.get("D:\\Projects\\Selenium\\Test\\index.html");
+    let driver = await new webdriver.Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(chromeOptions)
+        .build();
+
+    await driver.get("D:\\Projects\\Selenium\\\Test_Chrome\\index.html");
+
 
     // var webdriver = require('selenium-webdriver');
     // let driver3 = new Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
     // await driver3.get("D:\\Projects\\Selenium\\Test\\index.html");
 
     // type birthday
-    let birthdayBox =  driver3.findElement(By.id("date"));
+    let birthdayBox =  driver.findElement(By.id("date"));
     birthdayBox.sendKeys("08142001");
 
     // blur
-    driver3.findElement(By.id("right")).click();
+    driver.findElement(By.id("right")).click();
 
     // check if correct
-    let value = driver3.findElement(By.id("ageOutput")).getText();
+    let value = driver.findElement(By.id("ageOutput")).getText();
     value.then(function(result){
         if (result == "Your age is 20 years old!"){
             // console.log(result);
             points = points + 1;
-            console.log(points);
+            // console.log(points);
             // printPoints(points);
         }
     })
 
     // driver3.quit();
+    testNoEnterBirthday();
 
 };
 
 
 // function to test birthday field prompt when date not entered
 async function testNoEnterBirthday(){ 
+
+    // disable cluttering
+    const chromeOptions = new ChromeOptions();
+    chromeOptions.excludeSwitches('enable-logging');
+
     // open html
-    let driver4 = new Builder().forBrowser("chrome").build();
-    await driver4.get("D:\\Projects\\Selenium\\Test\\index.html");
+    let driver = await new webdriver.Builder()
+        .forBrowser("chrome")
+        .setChromeOptions(chromeOptions)
+        .build();
+
+    await driver.get("D:\\Projects\\Selenium\\\Test_Chrome\\index.html");
 
     // click birthday box
-    driver4.findElement(By.id("date")).click();
+    driver.findElement(By.id("date")).click();
 
     // blur
-    driver4.findElement(By.id("right")).click();
+    driver.findElement(By.id("right")).click();
 
     // check if correct
-    let value = driver4.findElement(By.id("ageOutput")).getText();
+    let value = driver.findElement(By.id("ageOutput")).getText();
     value.then(function(result){
         if (result == "Would you mind entering your birthday?"){
             // console.log(result);
@@ -100,8 +133,8 @@ function printPoints(points){
 
 // testHtmlOpen();
 testBirthdayClick();
-testEnterBirthday();
-testNoEnterBirthday();
+// testEnterBirthday();
+// testNoEnterBirthday();
 
 
 
