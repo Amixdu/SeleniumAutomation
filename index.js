9dc7a79df5d7d21a60cd67cd5ba8ad434e05f9b4
Amindu <86243749+Amixdu@users.noreply.github.com>
2021-11-15 13:59:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {Builder, Key, By} = require("selenium-webdriver");
-
+const {Builder, Key, By, ChromeOptions} = require("selenium-webdriver");
+var points = 0;
 
 
 async function testHtmlOpen (){ 
@@ -14,18 +14,32 @@ async function testBirthdayClick (){
     let driver2 = new Builder().forBrowser("chrome").build();
     await driver2.get("D:\\Projects\\Selenium\\Test\\index.html");
     await driver2.findElement(By.id("date")).click();
+
+    // check if correct
+    let value = driver2.findElement(By.id("ageOutput")).getText();
+    value.then(function(result){
+        if (result == "Lemme guess, your age is..."){
+            // console.log(result);
+            points = points + 1;
+            console.log(points);
+            // printPoints(points);
+        }
+    })
+
+    // driver2.quit();
 };
 
 
 // function to test birthday field prompt when date entered
-async function testEnterBirthday(){ 
-    // open html
-    // let driver3 = new Builder().forBrowser("chrome").build();
-    // await driver3.get("D:\\Projects\\Selenium\\Test\\index.html");
+async function testEnterBirthday(){
 
-    var webdriver = require('selenium-webdriver');
-    let driver3 = new Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+    // open html
+    let driver3 = new Builder().forBrowser("chrome").build();
     await driver3.get("D:\\Projects\\Selenium\\Test\\index.html");
+
+    // var webdriver = require('selenium-webdriver');
+    // let driver3 = new Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+    // await driver3.get("D:\\Projects\\Selenium\\Test\\index.html");
 
     // type birthday
     let birthdayBox =  driver3.findElement(By.id("date"));
@@ -37,9 +51,15 @@ async function testEnterBirthday(){
     // check if correct
     let value = driver3.findElement(By.id("ageOutput")).getText();
     value.then(function(result){
-        console.log(result);
+        if (result == "Your age is 20 years old!"){
+            // console.log(result);
+            points = points + 1;
+            console.log(points);
+            // printPoints(points);
+        }
     })
-    
+
+    // driver3.quit();
 
 };
 
@@ -56,34 +76,32 @@ async function testNoEnterBirthday(){
     // blur
     driver4.findElement(By.id("right")).click();
 
+    // check if correct
+    let value = driver4.findElement(By.id("ageOutput")).getText();
+    value.then(function(result){
+        if (result == "Would you mind entering your birthday?"){
+            // console.log(result);
+            points = points + 1;
+            console.log(points);
+            // printPoints(points);
+        }
+    })
+
+    // driver4.close();
+
 };
 
 
+function printPoints(points){
+    console.log(points);
+}
 
 
 
 // testHtmlOpen();
-// testBirthdayClick();
+testBirthdayClick();
 testEnterBirthday();
-// testNoEnterBirthday();
+testNoEnterBirthday();
 
 
-
-// var webdriver = require('selenium-webdriver');
-
-// var browser_name = new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build();
-
-// browser_name.get('http://www.google.com');
-
-// var promise = browser_name.getTitle();
-
-// promise.then(function(title) 
-
-// {
-
-// console.log(title);
-
-// });
-
-// browser_name.quit();
 
