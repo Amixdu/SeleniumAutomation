93cfcd8e517620250c1f73405e279e57f63e4d13
MethmaWijerathna <mminthaka_w@hotmail.com>
2021-11-22 11:24:13 +0530

    Provided mechanism to return path of file

--- tester.js
+++ tester.js
@@ -1,12 +1,10 @@
-function uploadFiles() {
+function runTester() {
     var files = document.getElementById('file_upload').files;
     if (files.length == 0) {
-        alert("Please first choose or drop any file(s)...");
+        alert("Please first choose or drop any file...");
         return;
     }
-    var filenames="";
-    for(var i=0;i<files.length;i++){
-        filenames+=files[i].name+"\n";
-    }
-    alert("Selected file(s) :\n____________________\n"+filenames);
-    }
+    var path = (window.URL || window.webkitURL).createObjectURL(files[0]);
+    alert(path);
+    // Function to run index.js
+ }
